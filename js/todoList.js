9e7020365d55ea25cf28c787deeae7516ea936b1
projecto2021 <duanly1987@gmail.com>
2022-02-13 00:29:09 +0100

    update data

--- js/todoList.js
+++ js/todoList.js
@@ -1,34 +1,32 @@
 import { setTarea } from "./funciones.js";
 const d = document;
-window.navigator
 
+export const TodoList = () => {
+  const $formulario = d.getElementById("formulario"),
+    $input = d.getElementById("input-id"),
+    $listaTarea = d.getElementById("lista-tareas"),
+    //$template = d.getElementById('template').content,
 
-export const TodoList = ()=>{
-const $formulario = d.getElementById("formulario"),
-  $input = d.getElementById("input-id"),
-  $listaTarea = d.getElementById("lista-tareas"),
-  //$template = d.getElementById('template').content,
-  
-  fragment = d.createDocumentFragment();
-  let tareas = {
-    34456788900:{
-      id:5553255555434,
-      texto:"tarea #1",
-      estado:false
-      
-    },
-    499939394949:{
-      id:1223354646546,
-      texto:"Tarea # 2",
-      estado:false
+    fragment = d.createDocumentFragment();
+    const data = {
+      122334556:{
+        id:1234454656,
+        texto:"Tarea #1",
+        estado:false,
+      },
+      1223934556:{
+        id:12344054656,
+        texto:"Tarea #2",
+        estado:false,
+      }
     }
-  }
-  
-  $formulario.addEventListener('submit',e=>{
-    e.preventDefault();
-    setTarea(e)
-    
-   
-  })
 
-}
+    console.log(data)
+
+  $formulario.addEventListener("submit", (e) => {
+    e.preventDefault();
+    setTarea(e);
+    $formulario.reset();
+    $input.focus();
+  });
+};
