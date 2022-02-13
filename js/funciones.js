4dd241fe28d02f3ca85bb06ec0679f6e4e88aa54
projecto2021 <duanly1987@gmail.com>
2022-02-13 03:02:36 +0100

    aputadate todo

--- js/funciones.js
+++ js/funciones.js
@@ -1,8 +1,43 @@
+"use strict";
 export const setTarea = (e) => {
-  const $input = document.getElementById('input-id')
-   if ($input.value.trim() === "") {
-     console.log("esta vacio");
-     return
-   }
-   console.log("diste click")
+  const $input = document.getElementById("input-id"),
+    $fragment = document.createDocumentFragment(),
+    $listaTarea = document.getElementById("listar-tareas"),
+    $template = document.getElementById("template").content;
+  if ($input.value.trim() === "") {
+    console.log("esta vacio");
+    return;
+  }
+  let data = {
+    122334556: {
+      id: 1234454656,
+      texto: "Tarea #1",
+      estado: false,
+    },
+    1223934556: {
+      id: 12344054656,
+      texto: "Tarea #2",
+      estado: false,
+    },
+  };
+  //console.log(data)
+  let tarea = {
+    id: Date.now(),
+    texto: $input.value,
+    estado: false,
+  };
+  tarea[data.id] = data;
+  //console.log(data)
+
+  
+  const pintarTareas = () => {
+    Object.values(data).forEach((el) => {
+      const clone = $template.cloneNode(true);
+      clone.querySelector("p").textContent = tarea.texto;
+      $fragment.appendChild(clone);
+    });
+    $listaTarea.innerHTML = ""
+    $listaTarea.appendChild($fragment)
+  };
+  pintarTareas()
 };
